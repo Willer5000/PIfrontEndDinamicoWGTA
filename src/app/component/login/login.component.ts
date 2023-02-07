ca6858168d669c657c8d615aa9b5f4e2aec7e00c
Willer <wtorrico@gmail.com>
2023-02-07 04:23:12 -0400

    se cambia login

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -9,6 +9,45 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(4)]]
+    });
+  }
+
+  ngOnInit(): void {}
+
+  get Email() {
+    return this.form.get('email');
+  }
+
+  get Password() {
+    return this.form.get('password');
+  }
+
+  onLogin() {
+    this.authService.Login(this.form.value).subscribe(data => {
+      console.log("DATA" + JSON.stringify(data));
+      this.router.navigate(['/home']);
+    });
+  }
+}
+
+//OPCION B
+/*import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/servicios/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
   form:FormGroup ;
   constructor(private formBuilder:FormBuilder, private authService:AuthService, private ruta:Router){
     this.form=this.formBuilder.group(
@@ -35,7 +74,7 @@ export class LoginComponent implements OnInit {
       })
     }
 }
-
+*/
 /*@Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
