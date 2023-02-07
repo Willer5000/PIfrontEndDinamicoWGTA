9a11ecef3a4575f2d293184e9e91d4a593c3bca0
Willer <wtorrico@gmail.com>
2023-02-07 02:52:55 -0400

    modificaciones style y guard

--- src/app/servicios/guard.guard.ts
+++ src/app/servicios/guard.guard.ts
@@ -7,6 +7,31 @@ import { AuthService } from './auth.service';
   providedIn: 'root'
 })
 export class GuardGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    let currentUser = this.authService.UsuarioAutenticado;
+    if (currentUser && currentUser.token) {
+      return true;
+    } else {
+      this.router.navigate(['/home']); // this.router.navigate(['/login']);
+      return false;
+    }
+  }
+}
+
+/*import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GuardGuard implements CanActivate {
  constructor(private authService:AuthService, private rutas:Router)
  {}
   canActivate(
@@ -23,3 +48,4 @@ export class GuardGuard implements CanActivate {
   }
 }
 }
+*/
