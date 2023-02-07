d65b9c246320a5b23f95054dccd4a77f642acf48
Willer <wtorrico@gmail.com>
2023-02-07 11:07:57 -0400

    se modifica app-routin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,28 @@ import { NewExperienciaComponent } from './component/portfolio/experiencia/new-e
 import { GuardGuard } from './servicios/guard.guard';
 
 const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'nuevaexp', component: NewExperienciaComponent},
+  {path:'editexp/:id', component: EditExperienciaComponent},
+  {path:'login', component: LoginComponent},
+  {path:'', redirectTo:'home', pathMatch:'full'}
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
+//OPCION B
+/*import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { LoginComponent } from './component/login/login.component';
+import { EditExperienciaComponent } from './component/portfolio/experiencia/edit-experiencia.component';
+import { NewExperienciaComponent } from './component/portfolio/experiencia/new-experiencia.component';
+import { GuardGuard } from './servicios/guard.guard';
+
+const routes: Routes = [
   {path:'home', component: HomeComponent, canActivate:[GuardGuard]},
   {path:'nuevaexp', component: NewExperienciaComponent},
   {path:'editexp/:id', component: EditExperienciaComponent},
@@ -18,6 +40,8 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+*/
+//OPCION C
 /*import { NgModule, Component } from '@angular/core';
 //import { NgModule, Component} from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
